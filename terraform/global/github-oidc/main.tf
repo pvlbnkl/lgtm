b55ca65e216002ce35a154303ca5d1009a5a36f0
Pavlo Bunakalia <pblo@tuta.io>
2024-05-27 19:34:00 -0400

    try a new role

--- terraform/global/github-oidc/main.tf
+++ terraform/global/github-oidc/main.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "github_oidc_assume" {
 }
 
 resource "aws_iam_role" "github_cicd" {
-  name               = "GithubActionsCICD"
+  name               = "lgtm-GHA"
   assume_role_policy = data.aws_iam_policy_document.github_oidc_assume.json
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/AdministratorAccess"
